5f2998e04ae0e79768d423c6671889798e9c627b
valentinFd <valikfad@gmail.com>
2022-03-14 12:10:31 +0200

    add user prompt for N and K

--- app/Driver.php
+++ app/Driver.php
@@ -6,8 +6,19 @@ use App\Program;
 
 class Driver
 {
-    public function start(int $N, int $K): void
+    public function start(): void
     {
+        do
+        {
+            $N = (int)readline('Enter N: ');
+            if ($N < 1) echo 'N must be greater than 0' . PHP_EOL;
+        } while ($N < 1);
+        do
+        {
+            $K = (int)readline('Enter K: ');
+            if ($K < 3) echo 'K must be greater than 2' . PHP_EOL;
+        } while ($K < 3);
+
         $program = new Program();
 
         $numbers = $program->numbers($N);
--- script.php
+++ script.php
@@ -5,4 +5,4 @@ require_once("vendor/autoload.php");
 use App\Driver;
 
 $driver = new Driver();
-$driver->start(20, 8);
+$driver->start();
